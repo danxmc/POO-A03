75e40d31cf13924b3990e85195c26bc23de9385b
danx_ <danx_@danx-pc>
2016-02-24 14:53:00 -0600

    Subir

---
+++ src/poo/relaciondecomposicion/Tecla.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package poo.relaciondecomposicion;
+
+/**
+ *
+ * @author DanielAlexandro
+ */
+public class Tecla {
+    //Atributos
+    private char digito;
+    
+    //Methods
+    //Constructor
+    public Tecla() {
+        
+    }
+    
+    public void setDigito(char d) {
+        this.digito = d;
+    }
+    
+    public char getDigito() {
+        return this.digito;
+    }
+}
